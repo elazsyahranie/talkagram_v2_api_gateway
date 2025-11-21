9e03717f70d2bc29388b54c39f0449e1cff0b03d
elazsyahranie <elazaribrahims@gmail.com>
2025-11-21 13:58:39 +0700

    fetching external api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { externalApiResponse, finalResultType } from './external-api.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/fetch')
+  async fetchExternalApi(
+    @Query() query: { results?: number; page?: number },
+  ): Promise<finalResultType[]> {
+    const result = await this.appService.fetchExternalApi(
+      query.results,
+      query.page,
+    );
+    return result;
+  }
 
   @Get()
   getHello(): string {
