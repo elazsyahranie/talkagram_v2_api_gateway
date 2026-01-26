6a18d3dcc1fe06dde3005792c2bd7951ff1f78a9
elazsyahranie <elazaribrahims@gmail.com>
2026-01-26 22:58:22 +0700

    store and seeders completed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,16 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient, Users } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 import * as bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
+// import { Knex, knex } from 'knex';
 
 const prisma = new PrismaClient();
 
 async function main() {
+  // Users Seeders
   const users: Prisma.UsersCreateInput[] = [];
   const password = await bcrypt.hash('123456', 10);
   for (let i = 0; i < 20; i++) {
-    // const name = faker.person.fullName();
     const first_name = faker.person.firstName();
     const middle_name = faker.person.middleName();
     const last_name = faker.person.lastName();
@@ -35,8 +37,25 @@ async function main() {
     data: users,
   });
 
-  // Add another one for stores!
-  // RALAT: Nanti dulu aja, selesaikan CRUD untuk stores dulu aja!
+  // Stores Seeders
+  // const stores: Prisma.StoresCreateInput[] = [];
+  for (let i = 0; i < 15; i++) {
+    const store_id = uuidv4();
+    const name = faker.company.name();
+
+    await prisma.stores.create({
+      data: { id: store_id, name, address: 'Jl. Raya Bogor' },
+    });
+
+    // Add store admin
+    const [userId] = await prisma.$queryRaw<
+      Users[]
+    >`SELECT id FROM "Users" ORDER BY RANDOM() LIMIT 1`;
+
+    await prisma.staffs.create({
+      data: { store_id, role: 'Admin', user_id: userId.id },
+    });
+  }
 }
 
 main()
